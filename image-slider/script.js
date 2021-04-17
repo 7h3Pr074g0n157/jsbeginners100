977d96cf011ee79e4e84eb17f8031771abdc19b5
n8trufe <n8trufe@gmail.com>
2021-04-17 20:29:10 +0200

    Delete console.log

--- image-slider/script.js
+++ image-slider/script.js
@@ -3,13 +3,11 @@
   function updateForward() {
     i >= 4 ? (i = 0) : i++;
     document.getElementById('image').src = `images/contBcg-${i}.jpg`;
-    console.log(i);
   }
 
   function updateBackward() {
     i <= 0 ? (i = 4) : i--;
     document.getElementById('image').src = `images/contBcg-${i}.jpg`;
-    console.log(i);
   }
 
   document.getElementById('backward').addEventListener('click', updateBackward);
